2a14d88bd3811da587aa82954beaca3632d44885
Shivansh270 <gupta.shivansh2709@gmail.com>
2023-08-17 14:10:34 +0530

    products section

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,6 +1,24 @@
 import "./Product.css";
-const Product = () => {
-    return <div>Product</div>;
+
+const Product = ({ data, id }) => {
+  //   const navigate = useNavigate();
+  return (
+    <div className="product-card">
+      <div className="thumbnail">
+        <img
+          src={
+            process.env.REACT_APP_STRIPE_APP_DEV_URL +
+            data?.img?.data[0]?.attributes?.url
+          }
+          //   alt={data.id}
+        />
+      </div>
+      <div className="prod-details">
+        <span className="name">{data.title}</span>
+        <span className="price">&#8377;{data.price}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
