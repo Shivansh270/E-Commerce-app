81ac4b479e3d56502b89cbb09184474b64b594ff
Shivansh270 <gupta.shivansh2709@gmail.com>
2023-08-17 16:07:51 +0530

    created custom hook

--- src/components/CategoryProducts/CategoryProducts.jsx
+++ src/components/CategoryProducts/CategoryProducts.jsx
@@ -1,10 +1,28 @@
+import { useParams } from "react-router-dom";
 import "./CategoryProducts.css";
-import React from 'react'
+import Products from "../Products/Products";
+import React from "react";
+import useFetch from "../../hooks/useFetch";
 
 const CategoryProducts = () => {
-  return (
-    <div>CategoryProducts</div>
-  )
-}
+  const { id } = useParams();
 
-export default CategoryProducts
+  const { data } = useFetch(
+    `/api/products?populate=*&[filters][categories][id]=${id}`
+  );
+  return (
+    <div className="category-main-content">
+      <div className="layout">
+        <div className="category-title">
+          {
+            data?.data?.[0]?.attributes?.categories?.data?.[0]?.attributes
+              ?.title
+          }
+        </div>
+        <Products innerPage={true} products={data} />
+      </div>
+    </div>
+  );
+};
+
+export default CategoryProducts;
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import { fetchDataFromApi } from "../utils/api";
+
+const useFetch = (endpoint) => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    makeApiCall();
+  }, [endpoint]);
+
+  const makeApiCall = async () => {
+    const response = await fetchDataFromApi(endpoint);
+    setData(response);
+  };
+  return { data };
+};
+
+export default useFetch;
